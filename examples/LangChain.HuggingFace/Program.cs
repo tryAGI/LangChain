fd134b7a786af62ceed56819c43eb37a14fdbdc6
Konstantin S <havendv@gmail.com>
2023-07-23 07:05:45 +0400

    example: Added HuggingFace example.

---
+++ examples/LangChain.HuggingFace/Program.cs
@@ -0,0 +1,8 @@
+﻿using LangChain.Providers;
+
+using var client = new HttpClient();
+var gpt2Model = new Gpt2Model(apiKey: string.Empty, client);
+
+var response = await gpt2Model.GenerateAsync("What would be a good company name be for name a company that makes colorful socks?");
+
+Console.WriteLine(response);
