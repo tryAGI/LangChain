0f14433fe77e8a6109bbd70869b993c5ca8cb0bf
Konstantin S <havendv@gmail.com>
2023-08-29 19:42:05 +0400

    example: Uncommented.

--- examples/LangChain.Samples.SequentialChain/Program.cs
+++ examples/LangChain.Samples.SequentialChain/Program.cs
@@ -1,38 +1,35 @@
-﻿// // See https://aka.ms/new-console-template for more information
-//
-// using LangChain.Chains.LLM;
-// using LangChain.Chains.Sequentials;
-// using LangChain.Providers;
-// using LangChain.Prompts;
-// using LangChain.Schema;
-//
-// using var httpClient = new HttpClient();
-// var llm = new Gpt4Model("api-key", httpClient);
-//
-// var firstTemplate = "What is a good name for a company that makes {product}?";
-// var firstPrompt = new PromptTemplate(new PromptTemplateInput(firstTemplate, new List<string>(1){"product"}));
-//
-// var chainOne = new LlmChain(new LlmChainInput(llm, firstPrompt)
-// {
-//     OutputKey = "company_name"
-// });
-//
-// var secondTemplate = "Write a 20 words description for the following company:{company_name}";
-// var secondPrompt = new PromptTemplate(new PromptTemplateInput(secondTemplate, new List<string>(1){"company_name"}));
-//
-// var chainTwo = new LlmChain(new LlmChainInput(llm, secondPrompt));
-//
-// var overallChain = new SequentialChain(new SequentialChainInput(new []
-// {
-//     chainOne,
-//     chainTwo
-// }, new []{"product"}));
-//
-// var result = await overallChain.CallAsync(new ChainValues(new Dictionary<string, object>(1)
-// {
-//     { "product", "colourful socks" }
-// }));
-//
-// // The result is an object with a `text` property.
-// Console.WriteLine(result.Value["text"]);
+﻿using LangChain.Chains.LLM;
+using LangChain.Chains.Sequentials;
+using LangChain.Providers;
+using LangChain.Prompts;
+using LangChain.Schema;
+
+using var httpClient = new HttpClient();
+var llm = new Gpt35TurboModel("api-key", httpClient);
+
+var firstTemplate = "What is a good name for a company that makes {product}?";
+var firstPrompt = new PromptTemplate(new PromptTemplateInput(firstTemplate, new List<string>(1){"product"}));
+
+var chainOne = new LlmChain(new LlmChainInput(llm, firstPrompt)
+{
+    OutputKey = "company_name"
+});
+
+var secondTemplate = "Write a 20 words description for the following company:{company_name}";
+var secondPrompt = new PromptTemplate(new PromptTemplateInput(secondTemplate, new List<string>(1){"company_name"}));
+
+var chainTwo = new LlmChain(new LlmChainInput(llm, secondPrompt));
+
+var overallChain = new SequentialChain(new SequentialChainInput(new []
+{
+    chainOne,
+    chainTwo
+}, new []{"product"}));
+
+var result = await overallChain.CallAsync(new ChainValues(new Dictionary<string, object>(1)
+{
+    { "product", "colourful socks" }
+}));
+
+Console.WriteLine(result.Value["text"]);
 Console.WriteLine("Test");
