9c340d4559b65bfae1dbdc2a490478700d5291c4
meelstorm <8780022+TesAnti@users.noreply.github.com>
2023-10-18 16:51:52 +0300

    changed LengthFunctionDelegate to Func<string, int>

---
+++ src/libs/LangChain.Core/Base/BaseLoader.cs
@@ -0,0 +1,13 @@
+﻿using LangChain.Docstore;
+
+namespace LangChain.Base;
+
+public abstract class BaseLoader
+{
+    public abstract List<Document> Load();
+
+    public List<Document> LoadAndSplit(TextSplitter textSplitter=null)
+    {
+        throw new NotImplementedException();
+    }
+}
