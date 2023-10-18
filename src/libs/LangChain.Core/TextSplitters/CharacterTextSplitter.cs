b3152fec962b0580906416b9e8de25070fdc350e
meelstorm <8780022+TesAnti@users.noreply.github.com>
2023-10-18 16:16:11 +0300

    ported CharacterTextSplitter - had to create and use TextSplitterBase class and Document class to match structure of original python implementation - i have not implemented methods which are relying on external source of information(from_huggingface_tokenizer,from_tiktoken_encoder) - added comments which are explaining logic behind implementation. it should help new people to understand what it does(original python implementation kind of hard to understand in some moments)

---
+++ src/libs/LangChain.Core/TextSplitters/CharacterTextSplitter.cs
@@ -0,0 +1,30 @@
+﻿using LangChain.Base;
+
+namespace LangChain.TextSplitters;
+
+/// <summary>
+/// Implementation of splitting text that looks at characters
+/// </summary>
+public class CharacterTextSplitter:TextSplitter
+{
+    private readonly string? _separator;
+
+    public CharacterTextSplitter(string? separator = "\n\n", int chunkSize = 4000, int chunkOverlap = 200, LengthFunctionDelegate? lengthFunction = null) : base(chunkSize, chunkOverlap, lengthFunction)
+    {
+        _separator = separator;
+    }
+
+    public override List<string> SplitText(string text)
+    {
+        List<string> splits;
+        if (_separator!=null)
+        {
+            splits = text.Split(new[] { _separator }, StringSplitOptions.None).ToList();
+        }
+        else
+        {
+            splits = new List<string> { text};
+        }
+        return this.MergeSplits(splits,_separator);
+    }
+}
