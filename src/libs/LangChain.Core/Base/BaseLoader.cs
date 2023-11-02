326a7f930f7a9101e51f1b42d938f68c7f462535
TesAnti <8780022+TesAnti@users.noreply.github.com>
2023-11-02 11:40:10 +0100

    feat: Added BaseLoader and TextLoader
    * Ported BaseLoader and TextLoader
    * added test project to solution

---
+++ src/libs/LangChain.Core/Base/BaseLoader.cs
@@ -0,0 +1,25 @@
+﻿using LangChain.Docstore;
+using LangChain.TextSplitters;
+
+namespace LangChain.Base;
+
+public abstract class BaseLoader
+{
+    public abstract List<Document> Load();
+
+    public List<Document> LoadAndSplit(TextSplitter textSplitter = null)
+    {
+        if (textSplitter == null)
+        {
+            textSplitter = new RecursiveCharacterTextSplitter();
+        }
+        var docs = Load();
+        return textSplitter.SplitDocuments(docs);
+    }
+
+    public virtual IEnumerable<Document> LazyLoad()
+    {
+        throw new NotImplementedException($"{GetType().Name} does not implement LazyLoad()");
+    }
+
+}
---
+++ src/tests/LangChain.Core.UnitTests/DocumentLoadesTests.cs
@@ -0,0 +1,18 @@
+using LangChain.DocumentLoaders;
+
+namespace LangChain.Core.UnitTests
+{
+    [TestClass]
+    public class DocumentLoadesTests
+    {
+        [TestMethod]
+        public void TextLoaderTest()
+        {
+            var filepath = Path.Combine(@"Resources", "state_of_the_union.txt");
+            var loader = new TextLoader(filepath);
+            var documents = loader.Load();
+            Assert.AreEqual(1, documents.Count);
+            
+        }
+    }
+}
