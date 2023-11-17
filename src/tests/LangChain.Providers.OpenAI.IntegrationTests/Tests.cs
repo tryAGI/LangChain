532bf4274d105f4b78944dd559cb1c033ab5b828
meelstorm <8780022+TesAnti@users.noreply.github.com>
2023-11-17 20:38:52 +0100

    test fix

--- src/tests/LangChain.Providers.OpenAI.IntegrationTests/Tests.cs
+++ src/tests/LangChain.Providers.OpenAI.IntegrationTests/Tests.cs
@@ -4,7 +4,7 @@ using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace LangChain.Providers.OpenAI.IntegrationTests;
 
-[TestFixture, Explicit, TestClass]
+[TestFixture, Explicit]
 public class GeneralTests
 {
     //     [TestMethod]
@@ -34,23 +34,23 @@ public class GeneralTests
     //
     //         Console.WriteLine(response.Messages.AsHistory());
     //     }
-    [TestMethod]
-#if CONTINUOUS_INTEGRATION_BUILD
-         [Ignore]
-#endif
-    public void SimpleTest()
-    {
-        var apiKey =
-            Environment.GetEnvironmentVariable("OPENAI_API_KEY") ??
-            throw new AssertInconclusiveException("OPENAI_API_KEY environment variable is not found.");
+//    [TestMethod]
+//#if CONTINUOUS_INTEGRATION_BUILD
+//         [Ignore]
+//#endif
+//    public void SimpleTest()
+//    {
+//        var apiKey =
+//            Environment.GetEnvironmentVariable("OPENAI_API_KEY") ??
+//            throw new AssertInconclusiveException("OPENAI_API_KEY environment variable is not found.");
 
-        var model = new Gpt35TurboModel(apiKey);
+//        var model = new Gpt35TurboModel(apiKey);
 
-        var prompt = "This is a test";
+//        var prompt = "This is a test";
 
-        var response = model.GenerateAsync(new ChatRequest(new []{prompt.AsHumanMessage()})).Result;
+//        var response = model.GenerateAsync(new ChatRequest(new []{prompt.AsHumanMessage()})).Result;
 
-        Assert.IsTrue(true);
-    }
+//        Assert.IsTrue(true);
+//    }
 
 }
