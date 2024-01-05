07c99d0db505243b21e3b30989f1721c7024c209
Ketan Khare <k.khare2418@outlook.com>
2024-01-05 15:54:25 +0530

    Added support for Azure OpenAI provider

--- examples/LangChain.Samples.Azure/Program.cs
+++ examples/LangChain.Samples.Azure/Program.cs
@@ -1,9 +1,8 @@
-﻿// using LangChain.Providers;
-// using LangChain.Providers.Azure;
-//
-// using var httpClient = new HttpClient();
-// var model = new Gpt35TurboModel("apiKey", "endpoint", new HttpClient());
-// var result = await model.GenerateAsync("What is a good name for a company that sells colourful socks?");
-//
-// Console.WriteLine(result);
-Console.WriteLine("Not implemented");
+﻿using LangChain.Providers;
+using LangChain.Providers.Azure;
+
+var model = new AzureOpenAIModel("AZURE_OPEN_AI_KEY", "ENDPOINT", "DEPLOYMENT_NAME");
+
+var result = await model.GenerateAsync("What is a good name for a company that sells colourful socks?");
+
+Console.WriteLine(result);
---
+++ src/libs/Providers/LangChain.Providers.Azure/AzureOpenAIConfiguration.cs
@@ -0,0 +1,49 @@
+﻿namespace LangChain.Providers
+{
+    /// <summary>
+    /// Configuration options for Azure OpenAI
+    /// </summary>
+    public class AzureOpenAIConfiguration
+    {
+        /// <summary>
+        /// Context size
+        /// How much tokens model will remember.
+        /// Most models have 2048
+        /// </summary>
+        public int ContextSize { get; set; } = 2048;
+
+        /// <summary>
+        /// Temperature
+        /// controls the apparent creativity of generated completions. 
+        /// Has a valid range of 0.0 to 2.0
+        /// Defaults to 1.0 if not otherwise specified.
+        /// </summary>
+        public float Temperature { get; set; } = 0.7f;
+
+        /// <summary>
+        /// Gets the maximum number of tokens to generate. Has minimum of 0.
+        /// </summary>
+        public int MaxTokens { get; set; } = 800;
+
+        /// <summary>
+        /// Number of choices that should be generated per provided prompt. 
+        /// Has a valid range of 1 to 128.
+        /// </summary>
+        public int ChoiceCount { get; set; } = 1;
+
+        /// <summary>
+        /// Azure OpenAI API Key
+        /// </summary>
+        public string? ApiKey { get; set; }
+
+        /// <summary>
+        /// Deployment name
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Azure OpenAI Resource URI
+        /// </summary>
+        public string Endpoint { get; set; }
+    }
+}
