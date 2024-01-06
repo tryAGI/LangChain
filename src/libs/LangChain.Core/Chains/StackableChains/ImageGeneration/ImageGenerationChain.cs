1d56dd119e673aafe45b696085f6737f64117f13
Konstantin S <havendv@gmail.com>
2024-01-07 02:27:28 +0400

    Update ImageGenerationChain.cs

--- src/libs/LangChain.Core/Chains/StackableChains/ImageGeneration/ImageGenerationChain.cs
+++ src/libs/LangChain.Core/Chains/StackableChains/ImageGeneration/ImageGenerationChain.cs
@@ -1,25 +1,41 @@
-﻿using LangChain.Abstractions.Schema;
+using LangChain.Abstractions.Schema;
 using LangChain.Chains.HelperChains;
 using LangChain.Providers;
 
 namespace LangChain.Chains.StackableChains.ImageGeneration;
 
+/// <summary>
+/// 
+/// </summary>
 public class ImageGenerationChain : BaseStackableChain
 {
     private readonly IGenerateImageModel _model;
 
-    public ImageGenerationChain(IGenerateImageModel model, string inputKey = "prompt", string outputKey = "image")
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="inputKey"></param>
+    /// <param name="outputKey"></param>
+    public ImageGenerationChain(
+        IGenerateImageModel model,
+        string inputKey = "prompt",
+        string outputKey = "image")
     {
         _model = model;
         InputKeys = new[] { inputKey };
         OutputKeys = new[] { outputKey };
-
     }
 
+    /// <inheritdoc />
     protected override async Task<IChainValues> InternalCall(IChainValues values)
     {
-        var prompt = values.Value[InputKeys[0]].ToString();
-        var image = await _model.GenerateImageAsBytesAsync(prompt);
+        values = values ?? throw new ArgumentNullException(nameof(values));
+        
+        var prompt =
+            values.Value[InputKeys[0]].ToString() ??
+            throw new InvalidOperationException("Input key is null");
+        var image = await _model.GenerateImageAsBytesAsync(prompt).ConfigureAwait(false);
         values.Value[OutputKeys[0]] = image;
         return values;
     }
