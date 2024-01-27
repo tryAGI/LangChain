27e268be701bcd35b8bf82941752c6a56368055e
Roderic Bos <github@rooc.nl>
2024-01-27 16:00:58 +0100

    feat: Azure Image Dall-e-3 setup (#118)

--- examples/LangChain.Samples.Azure/Program.cs
+++ examples/LangChain.Samples.Azure/Program.cs
@@ -6,3 +6,8 @@ var model = new AzureOpenAIModel("AZURE_OPEN_AI_KEY", "ENDPOINT", "DEPLOYMENT_NA
 var result = await model.GenerateAsync("What is a good name for a company that sells colourful socks?");
 
 Console.WriteLine(result);
+
+//Image test
+//var imageUrl = await model.GenerateImageAsUrlAsync("a blue horse called azure");
+//Console.WriteLine(model.RevisedPromptResult);
+//Console.WriteLine(imageUrl);
