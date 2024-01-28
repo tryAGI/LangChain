d17e1b4b79192c49f240ddb84b5481c26c212bca
TesAnti <8780022+TesAnti@users.noreply.github.com>
2024-01-28 13:32:44 +0100

    feat: Preparations for code-execution agents (#120)
    - Docker does not upload files, but attaches to a directory now. This allows to work with multiple files
    - Added test for c# project generation

--- src/libs/LangChain.Core/Chains/Chain.cs
+++ src/libs/LangChain.Core/Chains/Chain.cs
@@ -293,4 +293,11 @@ public static class Chain
     {
         return new CrewChain(allAgents, manager, inputKey, outputKey);
     }
+
+    public static ExtractCodeChain ExtractCode(
+        string inputKey = "text",
+        string outputKey = "code")
+    {
+        return new ExtractCodeChain(inputKey, outputKey);
+    }
 }
