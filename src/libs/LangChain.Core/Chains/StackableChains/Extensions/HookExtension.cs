c24435ac3a7d43bd7873fa1d53bf3d96af5843b2
meelstorm <8780022+TesAnti@users.noreply.github.com>
2024-02-01 16:49:31 +0100

    added hook for chain links

---
+++ src/libs/LangChain.Core/Chains/StackableChains/Extensions/HookExtension.cs
@@ -0,0 +1,13 @@
+﻿using LangChain.Abstractions.Schema;
+using LangChain.Chains.HelperChains;
+
+namespace LangChain.Chains.StackableChains.Extensions;
+
+public static class HookExtension
+{
+    public static T Hook<T>(this T chain, Action<IChainValues> hook) where T : BaseStackableChain
+    {
+        chain.SetHook(hook);
+        return chain;
+    }
+}
