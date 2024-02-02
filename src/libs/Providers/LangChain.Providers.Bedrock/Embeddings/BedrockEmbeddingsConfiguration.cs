72eeda3a0bec800551605108cfde43654f2095b8
Ty Augustine <127311+curlyfro@users.noreply.github.com>
2024-02-02 14:17:34 -0500

    feat: added AWS Bedrock provider and tests.
    Co-authored-by: Ty Augustine <afromobile@hotmail.com>
    
    TODO add Amazon Titan text and image models and Cohere models (#129)

---
+++ src/libs/Providers/LangChain.Providers.Bedrock/Embeddings/BedrockEmbeddingsConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Amazon;
+
+namespace LangChain.Providers.Bedrock.Embeddings;
+
+public class BedrockEmbeddingsConfiguration
+{
+    public string ModelId { get; set; }
+    public RegionEndpoint Region { get; set; } = RegionEndpoint.USEast1;
+}
