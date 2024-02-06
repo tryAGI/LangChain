ee9452ce7ffa59ee55eedc258c58b772831d6bb3
TesAnti <8780022+TesAnti@users.noreply.github.com>
2024-02-06 13:18:23 +0100

    feat: Unified agents tool (#134)

---
+++ src/libs/LangChain.Core/Chains/StackableChains/Agents/Tools/AgentTool.cs
@@ -0,0 +1,38 @@
+﻿namespace LangChain.Chains.StackableChains.Agents.Tools;
+
+/// <summary>
+/// 
+/// </summary>
+public abstract class AgentTool
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="description"></param>
+    protected AgentTool(
+        string name,
+        string? description = null)
+    {
+        Name = name;
+        Description = description ?? string.Empty;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public abstract Task<string> ToolTask(string input, CancellationToken token = default);
+}
