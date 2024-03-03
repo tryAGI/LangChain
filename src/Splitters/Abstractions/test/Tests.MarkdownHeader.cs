7e5a53c1edfa97550c2a19f4e69689d2411f6c7e
meelstorm <8780022+TesAnti@users.noreply.github.com>
2024-03-03 16:32:16 +0100

    Fixed the MarkdownHeaderTextSplitter and it's test to match closer to the original implementation (#140)

--- src/Splitters/Abstractions/test/Tests.MarkdownHeader.cs
+++ src/Splitters/Abstractions/test/Tests.MarkdownHeader.cs
@@ -2,6 +2,7 @@
 
 namespace LangChain.Splitters;
 
+[TestFixture]
 public partial class Tests
 {
     [Test]
@@ -35,7 +36,23 @@ some text
 ";
         var splitter = new MarkdownHeaderTextSplitter();
         var res = splitter.SplitText(md);
-        res.Count.Should().Be(1);
+        res.Count.Should().Be(2);
         res[0].Split("\n")[0].Should().Be("Header 1");
+        
+
+    }
+
+    [Test]
+    public void TestMarkdown3()
+    {
+        var md = "# Foo\n\n ## Bar\n\nHi this is Jim  \nHi this is Joe\n\n ## Baz\n\n Hi this is Molly";
+
+        var splitter = new MarkdownHeaderTextSplitter(includeHeaders: false);
+        var res = splitter.SplitText(md);
+        res[0].Should().Be("Hi this is Jim\nHi this is Joe");
+        res[1].Should().Be("Hi this is Molly");
+
+
+
     }
 }
