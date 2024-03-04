0d1b602bf2b055ac68b43df72db2901594432d6e
Ty Augustine <tyronea_2000@yahoo.com>
2024-03-04 13:46:59 -0500

    feat: Added Claude3 and Mistral models

--- src/Providers/Amazon.Bedrock/src/Predefined/Stability.cs
+++ src/Providers/Amazon.Bedrock/src/Predefined/Stability.cs
@@ -2,9 +2,5 @@
 namespace LangChain.Providers.Amazon.Bedrock.Predefined.Stability;
 
 /// <inheritdoc />
-public class StableDiffusionExtraLargeV0Model(BedrockProvider provider)
-    : StableDiffusionTextToImageModel(provider, id: "stability.stable-diffusion-xl-v0");
-
-/// <inheritdoc />
 public class StableDiffusionExtraLargeV1Model(BedrockProvider provider)
     : StableDiffusionTextToImageModel(provider, id: "stability.stable-diffusion-xl-v1");
