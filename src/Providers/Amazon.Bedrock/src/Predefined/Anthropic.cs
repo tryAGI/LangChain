8b358ba7ba54716ddd94a9edbeea587aaeb402b3
Ty Augustine <tyronea_2000@yahoo.com>
2024-04-01 19:31:04 +0000

    feat: updated Amazon Bedrock provider for accessKeyId and secretAccessKey (#183)
    * doc: Crew travel agent app
    
    * fix: minor fixes
    remove legacy Bedrock anthropic.claude-v1 model
    
    * feat: updated Amazon Bedrock provider for accessKeyId and secretAccessKey.  updated provider docs

--- src/Providers/Amazon.Bedrock/src/Predefined/Anthropic.cs
+++ src/Providers/Amazon.Bedrock/src/Predefined/Anthropic.cs
@@ -6,10 +6,6 @@ public class ClaudeInstantV1Model(BedrockProvider provider)
     : AnthropicClaudeChatModel(provider, id: "anthropic.claude-instant-v1");
 
 /// <inheritdoc />
-public class ClaudeV1Model(BedrockProvider provider)
-    : AnthropicClaudeChatModel(provider, id: "anthropic.claude-v1");
-
-/// <inheritdoc />
 public class ClaudeV2Model(BedrockProvider provider)
     : AnthropicClaudeChatModel(provider, id: "anthropic.claude-v2");
 
