7b6419b399ea4800ad60850b5e571b1931c7a408
Gunpal Jain <gunpal5@gmail.com>
2024-04-03 20:38:00 +0530

    fix: code cleanup

--- src/Providers/OpenAI/src/Chat/Message.Extension.cs
+++ src/Providers/OpenAI/src/Chat/Message.Extension.cs
@@ -1,37 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using OpenAI;
 using System.Text.Json.Nodes;
-using System.Threading.Tasks;
-using OpenAI;
 
-namespace LangChain.Providers.OpenAI
+// ReSharper disable once CheckNamespace
+namespace LangChain.Providers.OpenAI;
+
+public static class MessageExtensions
 {
-    public static class MessageExtensions
-    {
-        public static Message AsFunctionResultMessage(this string json, Tool tool)
-        {
-            return new Message(json, MessageRole.FunctionResult, $"{tool.Function.Name}:{tool.Id}");
-        }
+    private static readonly char[] Separator = [':'];
 
-        public static IReadOnlyList<Tool> ToToolCalls(this Message message)
+    public static Message AsFunctionResultMessage(this string json, Tool tool)
+    {
+        return new Message(json, MessageRole.FunctionResult, $"{tool.Function.Name}:{tool.Id}");
+    }
+
+    public static IReadOnlyList<Tool> ToToolCalls(this Message message)
+    {
+        var nameAndId = message.FunctionName?.Split(Separator, StringSplitOptions.RemoveEmptyEntries) ??
+                        throw new ArgumentException("Invalid functionCall name and id string");
+        
+        if (nameAndId.Length < 2)
+            throw new ArgumentException("Invalid functionCall name and id string");
+
+        return [
+                new Tool(new Function(nameAndId[0],
+                arguments: JsonNode.Parse(message.Content) ?? throw new ArgumentException("Invalid functionCall arguments")))
+                {
+                    Id = nameAndId[1]
+                }
+            ];
+    }
+    public static Tool GetTool(this Message message)
+    {
+        var nameAndId = message.FunctionName?.Split(Separator, StringSplitOptions.RemoveEmptyEntries) ??
+                        throw new ArgumentException("Invalid functionCall name and id string"); ;
+        if (nameAndId.Length < 2)
+            throw new ArgumentException("Invalid functionCall name and id string");
+        return new Tool(new Function(nameAndId[0]))
         {
-            var nameAndId = message.FunctionName?.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-            if (nameAndId.Length < 0)
-                throw new ArgumentException("Invalid functionCall name and id string");
-            return new[]
-                { new Tool(new Function(nameAndId[0], arguments: JsonNode.Parse(message.Content))) { Id = nameAndId[1] } };
-        }
-        public static Tool GetTool(this Message message)
-        {
-            var nameAndId = message.FunctionName?.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-            if (nameAndId.Length < 0)
-                throw new ArgumentException("Invalid functionCall name and id string");
-            return new Tool(new Function(nameAndId[0]))
-            {
-                Id = nameAndId[1]
-            };
-        }
+            Id = nameAndId[1]
+        };
     }
 }
