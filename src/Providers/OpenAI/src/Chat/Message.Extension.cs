92a648bd3b79cfa16e4246cb3293d61350d73b66
Gunpal Jain <gunpal5@gmail.com>
2024-04-03 20:29:59 +0530

    fix: Fixed OpenAI function calling with a temporary workaround.

---
+++ src/Providers/OpenAI/src/Chat/Message.Extension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using OpenAI;
+
+namespace LangChain.Providers.OpenAI
+{
+    public static class MessageExtensions
+    {
+        public static Message AsFunctionResultMessage(this string json, Tool tool)
+        {
+            return new Message(json, MessageRole.FunctionResult, $"{tool.Function.Name}:{tool.Id}");
+        }
+
+        public static IReadOnlyList<Tool> ToToolCalls(this Message message)
+        {
+            var nameAndId = message.FunctionName?.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            if (nameAndId.Length < 0)
+                throw new ArgumentException("Invalid functionCall name and id string");
+            return new[]
+                { new Tool(new Function(nameAndId[0], arguments: JsonNode.Parse(message.Content))) { Id = nameAndId[1] } };
+        }
+        public static Tool GetTool(this Message message)
+        {
+            var nameAndId = message.FunctionName?.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            if (nameAndId.Length < 0)
+                throw new ArgumentException("Invalid functionCall name and id string");
+            return new Tool(new Function(nameAndId[0]))
+            {
+                Id = nameAndId[1]
+            };
+        }
+    }
+}
