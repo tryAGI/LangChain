4bba2ce71238231bcb09977f4b4c0bd9e98aff39
meelstorm <8780022+TesAnti@users.noreply.github.com>
2024-04-12 11:24:56 +0200

    SQLite extension method for DI configuration

---
+++ src/Databases/Sqlite/src/ServeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LangChain.Serve.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LangChain.Databases.Sqlite
+{
+    public static class ServeExtensions
+    {
+        public static IServiceCollection AddSQLiteConversationRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IConversationRepository>(new SQLiteConversationRepository(connectionString));
+            return services;
+        }
+    }
+}
