35208044d80b05ae8e49905f4ffcfe1126d44e84
Daniel Jerez Garrido <danieljerezgarrido@gmail.com>
2024-05-15 13:04:30 +0200

    feat: add Groq provider (#303)

---
+++ src/Providers/GroqSharp/src/GroqSharpConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace LangChain.Providers.GroqSharp
+{
+    public class GroqSharpConfiguration
+    {
+        public const string SectionName = "GroqSharp";
+        public string ApiKey { get; set; }
+        public string ModelId { get; set; }
+        public double Temperature { get; set; } = 0.5;
+        public int MaxTokens { get; set; } = int.MaxValue;
+        public double TopP { get; set; } = 1.0;
+        public string Stop { get; set; } = "NONE";
+        public int StructuredRetryPolicy { get; set; } = 5;
+    }
+}
