110073605da8aaec1112d14a0385db6ace9b7e13
HavenDV <havendv@gmail.com>
2024-06-01 09:27:33 +0400

    test: Updated WikiTests.

--- src/Meta/test/WikiTests.cs
+++ src/Meta/test/WikiTests.cs
@@ -40,20 +40,30 @@ public class WikiTests
     [Test]
     public async Task AgentWithOllamaReact()
     {
-        var provider = new OllamaProvider(
-            options: new RequestOptions
-            {
-                Stop = new[] { "Observation", "[END]" }, // add injection word `Observation` and `[END]` to stop the model(just as additional safety feature)
-                Temperature = 0
-            });
-        var model = new OllamaChatModel(provider, id: "mistral:latest").UseConsoleForDebug();
+        // var provider = new OllamaProvider(
+        //     options: new RequestOptions
+        //     {
+        //         Stop = new[] { "Observation", "[END]" }, // add injection word `Observation` and `[END]` to stop the model(just as additional safety feature)
+        //         Temperature = 0
+        //     });
+        //var llm = new OllamaChatModel(provider, id: "mistral:latest").UseConsoleForDebug();
+        var apiKey =
+            Environment.GetEnvironmentVariable("OPENAI_API_KEY") ??
+            throw new InvalidOperationException("OpenAI API key is not set");
+        var llm = new Gpt35TurboModel(apiKey);
 
-        // create a google search tool
-        var searchTool = new GoogleCustomSearchTool(key: "<your key>", cx: "<your cx>", resultsLimit: 1);
+        // create a google search 
+        var searchApiKey =
+            Environment.GetEnvironmentVariable("GOOGLE_SEARCH_API_KEY") ??
+            throw new InvalidOperationException("GOOGLE_SEARCH_API_KEY is not set");
+        var cx =
+            Environment.GetEnvironmentVariable("GOOGLE_SEARCH_CX") ??
+            throw new InvalidOperationException("GOOGLE_SEARCH_CX is not set");
+        var searchTool = new GoogleCustomSearchTool(key: searchApiKey, cx: cx, resultsLimit: 1);
 
         var chain =
             Set("What is tryAGI/LangChain?")
-            | ReActAgentExecutor(model) // does the magic
+            | ReActAgentExecutor(llm) // does the magic
                 .UseTool(searchTool); // add the google search tool
 
         await chain.RunAsync();
