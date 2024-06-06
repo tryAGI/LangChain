08d535a865623ffbe0a9fa5e7f6e5ba9342deffd
Gunpal Jain <gunpal5@gmail.com>
2024-06-06 22:50:43 +0530

    feat(GoogleGemini): Added CountTokens fix: fixed usages

---
+++ src/Providers/Google/src/GoogleChatModel.Tokens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenerativeAI.Types;
+
+namespace LangChain.Providers.Google
+{
+    public partial class GoogleChatModel
+    {
+        public async Task<int> CountTokens(string text)
+        {
+            return await CountTokens(new Message[] { new Message(text, MessageRole.Human) }).ConfigureAwait(false);
+        }
+
+        public async Task<int> CountTokens(IEnumerable<Message> messages)
+        { 
+            var response = await this.Api.CountTokens(new CountTokensRequest() { Contents = messages.Select(ToRequestMessage).ToArray() }).ConfigureAwait(false);
+
+            return response.TotalTokens;
+        }
+    }
+}
