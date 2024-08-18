bf0608e4af4b74ccc5d88e729cb743fc2cf1d3c1
HavenDV <havendv@gmail.com>
2024-08-19 00:06:01 +0400

    docs: Added WikiTests from Meta integration tests to documentation.

--- src/Helpers/GenerateDocs/Program.cs
+++ src/Helpers/GenerateDocs/Program.cs
@@ -17,6 +17,28 @@ foreach (var path in Directory.EnumerateFiles(sampleDirectory, "Program.cs", Sea
 ```");
 }
 
+var metaTestsFolder = Path.Combine(solutionDirectory, "src", "Meta", "test");
+Console.WriteLine($"Generating samples from {metaTestsFolder}...");
+foreach (var path in Directory.EnumerateFiles(metaTestsFolder, "WikiTests.*.cs", SearchOption.AllDirectories))
+{
+    var code = await File.ReadAllTextAsync(path);
+
+    var newDir = Path.Combine(solutionDirectory, "docs", "samples");
+    Directory.CreateDirectory(newDir);
+    
+    var start = code.IndexOf("\n    {", StringComparison.Ordinal);
+    var end = code.IndexOf("\n    }", StringComparison.Ordinal);
+    code = code.Substring(start + 4, end - start + 4);
+    
+    var lines = code.Split('\n')[1..^2];
+    code = string.Join('\n', lines.Select(x => x.Length > 8 ? x[8..] : string.Empty));
+    
+    var newPath = Path.Combine(newDir, $"{Path.GetExtension(Path.GetFileNameWithoutExtension(path)).TrimStart('.')}.md");
+    await File.WriteAllTextAsync(newPath, $@"```csharp
+{code}
+```");
+}
+
 var mkDocs = await File.ReadAllTextAsync(mkDocsPath);
 var newMkDocs = mkDocs.Replace(
     "# EXAMPLES #",
