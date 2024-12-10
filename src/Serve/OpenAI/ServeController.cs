12de18111a01a935516052c8ce9a28ca16c35d41
Konstantin S <havendv@gmail.com>
2024-12-10 20:36:04 +0400

    fix: Fixed issue with Serve/OpenAI

--- src/Serve/OpenAI/ServeController.cs
+++ src/Serve/OpenAI/ServeController.cs
@@ -1,43 +1,43 @@
-using System.Text.Json.Serialization;
-using OpenAI;
-
-namespace LangChain.Serve.OpenAI;
-
-public class ServeController(
-    ServeOptions options)
-{
-    public ModelsList GetModel()
-    {
-        return new ModelsList
-        {
-            Models = options.ListModels().Select(static x => new Model12
-            {
-                Object = ModelObject.Model,
-                Created = DateTimeOffset.UtcNow,
-                Id = x,
-                OwnedBy = "OpenAI",
-            }).ToList()
-        };
-    }
-
-    public ModelsList ListModels()
-    {
-        return new ModelsList
-        {
-            Models = options.ListModels().Select(static x => new Model12
-            {
-                Object = ModelObject.Model,
-                Created = DateTimeOffset.UtcNow,
-                Id = x,
-                OwnedBy = "OpenAI",
-            }).ToList()
-        };
-    }
-}
-
-public sealed class ModelsList
-{
-    [JsonInclude]
-    [JsonPropertyName("data")]
-    public IReadOnlyCollection<Model12> Models { get; set; } = [];
-}
+// using System.Text.Json.Serialization;
+// using OpenAI;
+//
+// namespace LangChain.Serve.OpenAI;
+//
+// public class ServeController(
+//     ServeOptions options)
+// {
+//     public ModelsList GetModel()
+//     {
+//         return new ModelsList
+//         {
+//             Models = options.ListModels().Select(static x => new Model12
+//             {
+//                 Object = ModelObject.Model,
+//                 Created = DateTimeOffset.UtcNow,
+//                 Id = x,
+//                 OwnedBy = "OpenAI",
+//             }).ToList()
+//         };
+//     }
+//
+//     public ModelsList ListModels()
+//     {
+//         return new ModelsList
+//         {
+//             Models = options.ListModels().Select(static x => new Model12
+//             {
+//                 Object = ModelObject.Model,
+//                 Created = DateTimeOffset.UtcNow,
+//                 Id = x,
+//                 OwnedBy = "OpenAI",
+//             }).ToList()
+//         };
+//     }
+// }
+//
+// public sealed class ModelsList
+// {
+//     [JsonInclude]
+//     [JsonPropertyName("data")]
+//     public IReadOnlyCollection<Model15> Models { get; set; } = [];
+// }
